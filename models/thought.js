d4adaefcdde5ffacd8d123b5f42fdbdd9dc8e157
Sara Garcia <saragca81@gmail.com>
2023-11-06 14:57:21 -0800

    added code

--- models/thought.js
+++ models/thought.js
@@ -1,9 +1,40 @@
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const { Schema , models, Types } = require('mongoose');
 
-const BlogPost = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
-});
+const moment = require('moment');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId,
+        }
+    },
+    reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+ thoughtSchema.virtual('reactionCount')
+ .get(function() {
+    return this.reaction.length;
+ })
+
+ const Thought = models('thought', thoughtSchema);
+ module.exports = Thought;
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const { Schema, models, Types} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            require: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            match: 
+        }
+    }
+)
